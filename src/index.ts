1c15e7693a8125ad338e5a1a759486aa223ae358
bryanadamson <adamsontaofeeq@gmail.com>
2023-06-17 21:30:22 +0100

    deployment
    railway deployment 3

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,18 @@
 import express, {Express} from "express";
-import {cookieSecret, mongoURI, port} from "./utils/constants";
+import {mongoURI, port} from "./utils/constants";
 import mongoose from "mongoose";
 import AuthRoutes from "./routes/AuthRoutes";
 import "./configs/Passport";
 import passport from "passport";
 import ErrorRoutes from "./routes/ErrorRoutes";
-import session from "express-session";
 import UserRoutes from "./routes/UserRoutes";
 import CustomerRoutes from "./routes/CustomerRoutes";
 import OrderRoutes from "./routes/OrderRoutes";
 import customers from "./middleware/customers";
 import authenticate from "./middleware/authenticate";
-import MongoStore from "connect-mongo";
 
 
 const app: Express = express();
-
-app.use(session({
-    secret: cookieSecret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true },
-    store: new MongoStore({
-        mongoUrl: mongoURI
-    })
-}));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
